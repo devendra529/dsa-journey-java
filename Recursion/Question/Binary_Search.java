7f5c84bd77c935be67e9a6643a23ad89482f2c5d
devendra529 <singhdevendrapratap529@gmail.com>
2026-06-15 15:30:06 +0530

    adding the recursion basic question

---
+++ Recursion/Question/Binary_Search.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Binary_Search {
+    static int searching(int arr[], int targert){
+        Arrays.sort(arr);
+        int low =arr[0];
+        int high= arr[]
+    }
+}
---
+++ Recursion/Question/Print_n_natural_numbers.java
@@ -0,0 +1,16 @@
+// printing the first n natural number using the recursion
+//n=5
+
+public class Print_n_natural_numbers{
+    static void recursion(int n){
+         if(n==0){
+            return;
+         }
+         System.out.print(n);
+         recursion(n-1);
+    }
+    public static void main(String[]args){
+        int n=5;
+        recursion(n);
+    }
+}
